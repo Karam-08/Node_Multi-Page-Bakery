cba5c83082d34e151680cf81de5cee68b42b8498
Karam Abbas <kma69362@gmail.com>
2025-08-27 13:18:19 -0700

    commit

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public'))),
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-},
-app.listen(5000, () =>{console.log('Server is running on http://localhost:5000')}))
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Homepage route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+// Start server
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000')
+})
